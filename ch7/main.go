733b439c086e8d0dafb6abeb5bcbac9557cebe0d
Alejandro Guirao <lekumberri@gmail.com>
2017-04-10 08:15:03 +0200

    Add example of Writer interface

---
+++ ch7/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c) // "5", = len("hello")
+
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c) // "12", = len("hello, Dolly")
+}
