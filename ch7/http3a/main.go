283ac07be83a1807a297c394007c0e6e22db6e6d
Alejandro Guirao <lekumberri@gmail.com>
2017-04-24 09:18:49 +0200

    Add http handler example

---
+++ ch7/http3a/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/list", db.list)
+	mux.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
+
+type database map[string]dollars
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+func (db database) list(w http.ResponseWriter, r *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		http.Error(w, fmt.Sprintf("no such item: %q\n", item), http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
