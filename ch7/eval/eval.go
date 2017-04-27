a3460742a605052275d86b528959ac97d903073d
Alejandro Guirao <lekumberri@gmail.com>
2017-04-27 08:29:02 +0200

    Add Eval implementations

--- ch7/eval/eval.go
+++ ch7/eval/eval.go
@@ -1,5 +1,10 @@
 package eval
 
+import (
+	"fmt"
+	"math"
+)
+
 // An Expr is an arithmetic expression.
 type Expr interface {
 	// Eval returns the value of this Expr in the environment env.
@@ -29,3 +34,48 @@ type call struct {
 
 // The Env maps variables names to values
 type Env map[Var]float64
+
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+
+func (l literal) Eval(_ Env) float64 {
+	return float64(l)
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+
+	}
+	panic(fmt.Sprintf("unsupported function call: %q", c.fn))
+}
