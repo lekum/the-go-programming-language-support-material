836549e2170a2ed645a55f943bbf0154fc0659ff
Alejandro Guirao <lekumberri@gmail.com>
2017-05-03 10:32:38 +0200

    Add example of Type assertions

---
+++ ch7/assertions/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	var w io.Writer
+	w = os.Stdout
+	f, ok := w.(*os.File) // success
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Interface %T is not of type *os.File\n", w)
+	}
+	b, ok := w.(*bytes.Buffer) // failure
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Interface %T is not of type *bytes.Buffer\n", w)
+	}
+	fmt.Println(f, b)
+}
