c06d3e582b6c816c4695f3ebda3241e22591dd7c
Alejandro Guirao <lekumberri@gmail.com>
2017-05-24 13:25:32 +0200

    Add -depth

--- ch8/crawl4/main.go
+++ ch8/crawl4/main.go
@@ -5,42 +5,53 @@ import (
 	"fmt"
 	"gopl.io/ch5/links"
 	"log"
-	"os"
+	"strings"
 )
 
 // tokens is a counting semaphore used to enforce
 // a limit of 20 concurrent requests
 var tokens = make(chan struct{}, 20)
 
+// worklist of urls list (may contain duplicates)
+type worklist struct {
+	currentDepth int
+	urls         []string
+}
+
+// unseen url
+type unseenLink struct {
+	currentDepth int
+	url          string
+}
+
 func crawl(url string) []string {
-	fmt.Println(url)
 	tokens <- struct{}{} // acquire a token
 	list, err := links.Extract(url)
 	<-tokens // release the token
 	if err != nil {
-		log.Print(err)
+		log.Print("Error extracting links: ", err)
 	}
 	return list
 }
 
 func main() {
-	var depth = flag.Int("depth", 100, "max depth of crawling")
+	var maxDepth = flag.Int("depth", 100, "max depth of crawling")
 	flag.Parse()
-	worklist := make(chan []string)  // lists of URLs, may have duplicates
-	unseenLinks := make(chan string) // de-duplicated URLs
+	wl := make(chan worklist)
+	ul := make(chan unseenLink)
 
 	// Add command-line arguments to worklist.
 	go func() {
-		worklist <- os.Args[1:]
+		wl <- worklist{0, flag.Args()} // currentDepth=0
 	}()
 
 	// Create 20 crawler goroutines to fetch each unseen link
 	for i := 0; i < 20; i++ {
 		go func() {
-			for link := range unseenLinks {
-				foundLinks := crawl(link)
+			for link := range ul {
+				foundLinks := crawl(link.url)
 				go func() {
-					worklist <- foundLinks
+					wl <- worklist{link.currentDepth + 1, foundLinks}
 				}()
 			}
 		}()
@@ -49,11 +60,16 @@ func main() {
 	// The main goroutine de-duplicates worklist items
 	// and sends the unseen ones to the crawlers.
 	seen := make(map[string]bool)
-	for list := range worklist {
-		for _, link := range list {
+	for list := range wl {
+		if list.currentDepth >= *maxDepth {
+			// maxDepth reached, ignoring list
+			continue
+		}
+		for _, link := range list.urls {
 			if !seen[link] {
 				seen[link] = true
-				unseenLinks <- link
+				fmt.Printf("%s%s\n", strings.Repeat("\t", list.currentDepth), link)
+				ul <- unseenLink{list.currentDepth, link}
 			}
 		}
 	}
