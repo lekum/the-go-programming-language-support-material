4b735deed48df421d359fcc575bdbba1ed987dea
Alejandro Guirao <lekumberri@gmail.com>
2017-05-25 09:04:52 +0200

    Add select{} example

---
+++ ch8/countdown/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func launch() {
+	fmt.Println("Fire!")
+}
+
+func main() {
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		abort <- struct{}{}
+	}()
+	fmt.Println("Commencing countdown. Press return to abort.")
+	tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+		select {
+		case <-tick:
+		// Do nothing
+		case <-abort:
+			fmt.Println("Launch aborted!")
+			return
+		}
+	}
+	launch()
+}
