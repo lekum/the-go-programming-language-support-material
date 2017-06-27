95cee8542fde7e1ddfe9b5421cd5a4a36027ddc4
Alejandro Guirao <lekumberri@gmail.com>
2017-06-27 08:59:56 +0200

    Add new tests

--- ch11/word1/word.go
+++ ch11/word1/word.go
@@ -1,11 +1,20 @@
 // Package word provides utilities for word games.
 package word
 
+import "unicode"
+
 // IsPalindrome reports wether s reads the same forward and backward.
+// Letter case is ignored, as are non-letters.
 // (Our first attempt.)
 func IsPalindrome(s string) bool {
-	for i := range s {
-		if s[i] != s[len(s)-1-i] {
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
 	}
