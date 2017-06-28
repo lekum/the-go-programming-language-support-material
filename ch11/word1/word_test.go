126ced6949b6225d5ce12b459c9282fb53c17a36
Alejandro Guirao <lekumberri@gmail.com>
2017-06-28 08:38:40 +0200

    Refactor tests into 'table' style

--- ch11/word1/word_test.go
+++ ch11/word1/word_test.go
@@ -2,30 +2,30 @@ package word
 
 import "testing"
 
-func TestPalindrome(t *testing.T) {
-	if !IsPalindrome("detartrated") {
-		t.Error(`IsPalindrome("detartrated") = false`)
+func TestIsPalindrom(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false}, // non-palindrome
+		{"desserts", false},
+		// semi-palindrome
 	}
-	if !IsPalindrome("kayak") {
-		t.Error(`IsPalindrome("kayak") = false`)
-	}
-}
 
-func TestNotPalindrome(t *testing.T) {
-	if IsPalindrome("palindrome") {
-		t.Error(`IsPalindrome("palindrome") = true`)
-	}
-}
-
-func TestFrenchPalindrome(t *testing.T) {
-	if !IsPalindrome("été") {
-		t.Error(`IsPalindrome("été") = false`)
-	}
-}
-
-func TestCanalPalindrome(t *testing.T) {
-	input := "A man, a plan, a canal: Panama"
-	if !IsPalindrome(input) {
-		t.Errorf(`IsPalindrome(%q) = false`, input)
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf(`IsPalindrome(%q) = %t`, test.input, got)
+		}
 	}
 }
