6c5af9c919a5b255ae5d4edf5dc92450cc89a04e
Alejandro Guirao <lekumberri@gmail.com>
2017-07-04 09:03:07 +0200

    Add example of test faking

---
+++ ch11/storage1/storage.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+func bytesInUse(username string) int64 { return 0 /* ... */ }
+
+// Email sender configuration
+const sender = "notifications@example.com"
+const password = "correctorsebatterystaple"
+const hostname = "smtp.example.com"
+
+const template = `Warning: you are using %d bytes of storage,
+%d%% of your quota.`
+
+func CheckQuota(username string) {
+	used := bytesInUse(username)
+	const quota = 1000000000 // 1 GB
+	percent := 100 * used / quota
+	if percent < 90 {
+		return // OK
+	}
+	msg := fmt.Sprintf(template, used, percent)
+	notifyUser(username, msg)
+}
+
+var notifyUser = func(username, msg string) {
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname+":587", auth, sender,
+		[]string{username}, []byte(msg))
+	if err != nil {
+		log.Printf("smtp.SendMail(%s) failed: %v", username, err)
+	}
+
+}
---
+++ ch11/storage1/storage_test.go
@@ -0,0 +1,33 @@
+package storage
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCheckQuotaNotifiesUser(t *testing.T) {
+
+	// Save and restore original notifyUser
+	saved := notifyUser
+	defer func() { notifyUser = saved }()
+
+	// Install the test's fake notifyUser.
+	var notifiedUser, notifiedMsg string
+	notifyUser = func(user, msg string) {
+		notifiedUser, notifiedMsg = user, msg
+	}
+	// ...simulate a 980MB-used condition...
+	const user = "joe@example.org"
+	CheckQuota(user)
+	if notifiedUser == "" && notifiedMsg == "" {
+		t.Fatalf("notifyUser not called")
+	}
+	if notifiedUser != user {
+		t.Errorf("wrong user (%s) notified, want %s",
+			notifiedUser, user)
+	}
+	const wantSubstring = "98% of your quota"
+	if !strings.Contains(notifiedMsg, wantSubstring) {
+		t.Errorf("unexpected notification message <<%s>>, "+"want substring %q", notifiedMsg, wantSubstring)
+	}
+}
